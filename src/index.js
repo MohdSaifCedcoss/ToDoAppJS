17a9f78a4690117b6197bd099fbb884be2e1236b
MohdSaifCedcoss <mohdsaif2022aug@cedcoss.com>
2022-09-13 18:36:56 +0530

    Made some changes for delete function

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ function addTask() {
   var ob = {
     task: task,
     flag: 0,
+    uid: Math.ceil(Math.random()*100)
   };
   tasks.push(ob);
+  console.log(tasks);
   display();
 }
 
@@ -22,13 +24,13 @@ function display() {
     if (tasks[i].flag == 0) {
       str +=
         '<input type="checkbox" onclick="showComplete(\'' +
-        tasks[i].task +
+        tasks[i].uid +
         "')\">" +
         tasks[i].task +
         '<input type="button" onclick="edit(\'' +
-        tasks[i].task +
+        tasks[i].uid+ 
         '\')" value="Edit"><input type="button" onclick="delFun(\'' +
-        tasks[i].task +
+        tasks[i].uid+
         '\')" value="Delete"><br>';
     }
   }
@@ -39,7 +41,7 @@ function display() {
 //function to remove checked tasks from incomplete array to add them to array of completed tasks
 function showComplete(val) {
   for (let i = 0; i < tasks.length; i++) {
-    if (tasks[i].task == val) {
+    if (tasks[i].uid == val) {
       compleTasks.push(tasks[i]);
       tasks.splice(i, 1);
     }
@@ -58,15 +60,15 @@ function displayCompleted() {
       // console.log("hey in for " + tasks[i].task);
       str +=
         '<input type="checkbox"onclick="showComplete2(\'' +
-        compleTasks[i].task +
+        compleTasks[i].uid +
         "')\" checked>" +
         '<h4 style="text-decoration:line-through;display:inline">' +
         compleTasks[i].task +
         "</h4>" +
         '<input type="button" onclick="edit2(\'' +
-        compleTasks[i].task +
+        compleTasks[i].uid +
         '\')" value="Edit"><input type="button" onclick="delFun2(\'' +
-        compleTasks[i].task +
+        compleTasks[i].uid +
         '\')" value="Delete"><br>';
     }
   }
@@ -75,8 +77,9 @@ function displayCompleted() {
 
 //function to delete tasks of incomplete tasks array
 function delFun(val) {
+  // console.log("in del "+val.uid);
   for (let i = 0; i < tasks.length; i++) {
-    if (tasks[i].task == val) {
+    if (tasks[i].uid == val) {
       tasks.splice(i, 1);
     }
   }
@@ -86,7 +89,7 @@ function delFun(val) {
 //function to delete tasks of complete tasks array
 function delFun2(val) {
   for (let i = 0; i < compleTasks.length; i++) {
-    if (compleTasks[i].task == val) {
+    if (compleTasks[i].uid == val) {
       compleTasks.splice(i, 1);
     }
   }
@@ -95,14 +98,14 @@ function delFun2(val) {
 
 
 //function to edit/update the value of old incomplete tasks
-function edit(val) {
-  document.getElementById("new-task").value = val;
+function edit(uid) {
+  // document.getElementById("new-task").value = val;
   document.getElementById("addButton").style.display = "none";
   document.getElementById("updateButton").style.display = "inline";
   document.getElementById("updateButton").onclick = function () {
     let new_val = document.getElementById("new-task").value;
     for (let i = 0; i < tasks.length; i++) {
-      if (tasks[i].task == val) {
+      if (tasks[i].uid == uid) {
         tasks[i].task = new_val;
         tasks[i].flag = 0;
       }
@@ -115,13 +118,13 @@ function edit(val) {
 
 //function to edit/update the value of old completed tasks
 function edit2(val) {
-  document.getElementById("new-task").value = val;
+  // document.getElementById("new-task").value = val;
   document.getElementById("addButton").style.display = "none";
   document.getElementById("updateButton").style.display = "inline";
   document.getElementById("updateButton").onclick = function () {
     let new_val = document.getElementById("new-task").value;
     for (let i = 0; i < compleTasks.length; i++) {
-      if (compleTasks[i].task == val) {
+      if (compleTasks[i].uid == val) {
         compleTasks[i].task = new_val;
         compleTasks[i].flag = 0;
       }
@@ -135,7 +138,7 @@ function edit2(val) {
 //function to remove tasks from completed tasks and add them in incomplete tasks array
 function showComplete2(val) {
   for (let i = 0; i < compleTasks.length; i++) {
-    if (compleTasks[i].task == val) {
+    if (compleTasks[i].uid == val) {
       tasks.push(compleTasks[i]);
       compleTasks.splice(i, 1);
     }
